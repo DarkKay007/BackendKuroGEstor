0d4c9008786227719cb1522838846ac1931a9509
Kai <159480405+DarkKay007@users.noreply.github.com>
2024-06-07 12:01:27 -0500

    Update app.js

--- app/app.js
+++ app/app.js
@@ -42,7 +42,7 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/", routes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000; // Changed fallback port to 3000
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
