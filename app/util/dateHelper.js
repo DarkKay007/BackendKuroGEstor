b431833dc65b5500af560130dde37975c4d1b94b
Kai <159480405+DarkKay007@users.noreply.github.com>
2024-06-07 03:43:13 -0500

    Update dateHelper.js

--- app/util/dateHelper.js
+++ app/util/dateHelper.js
@@ -1,7 +1,12 @@
-// dateHelper.js
-
+// util/dateHelper.js
 export const getCurrentDateTime = () => {
-    const currentDate = new Date();
-    return currentDate.toISOString(); // Retorna la fecha y hora actual en formato ISO (YYYY-MM-DDTHH:mm:ss.sssZ)
-  };
-  
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, '0');
+  const day = String(now.getDate()).padStart(2, '0');
+  const hours = String(now.getHours()).padStart(2, '0');
+  const minutes = String(now.getMinutes()).padStart(2, '0');
+  const seconds = String(now.getSeconds()).padStart(2, '0');
+
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+};
