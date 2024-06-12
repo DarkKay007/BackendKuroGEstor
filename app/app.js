7133547bbe1ed75525d9082cd6b1cbecefc41c04
Kai <159480405+DarkKay007@users.noreply.github.com>
2024-06-12 17:09:12 -0500

    Update app.js

--- app/app.js
+++ app/app.js
@@ -20,9 +20,15 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
 
+// Otro middleware y configuraciones de tu servidor
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/", routes);
 
-export default app;
+// Escuchar en un puerto específico
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
