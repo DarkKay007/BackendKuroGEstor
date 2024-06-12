83ea37fcabacacbe65d5b1de231c308ddab394ad
Kai <159480405+DarkKay007@users.noreply.github.com>
2024-06-12 15:05:45 -0500

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,18 +1,29 @@
 import express from 'express';
-import cors from 'cors';
 import routes from './routes/index.js';
+import cors from 'cors';
 
 const app = express();
 
-// Configurar CORS para permitir solicitudes desde el dominio de Vercel
+// Middleware to enable CORS for specific origin
 app.use(cors({
-  origin: 'https://kuro-gestor.vercel.app' // Reemplaza esto con la URL de tu frontend en Vercel
+  origin: 'https://kuro-gestor.vercel.app', // Allow only this origin
+  credentials: true
 }));
+
+// Middleware to handle OPTIONS requests
+app.options('*', cors());
+
+// Setting headers for all responses
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://kuro-gestor.vercel.app'); // Allow only this origin
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-// Define rutas
 app.use("/", routes);
 
 export default app;
