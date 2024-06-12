f6d4c5e4edf38e3df700039d3f77561c624df121
Kai <159480405+DarkKay007@users.noreply.github.com>
2024-06-12 17:15:36 -0500

    Update app.js

--- app/app.js
+++ app/app.js
@@ -14,21 +14,25 @@ const corsOptions = {
       callback(new Error('Not allowed by CORS'));
     }
   },
-  credentials: true,
+  credentials: true, // Allow credentials
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allow these methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allow these headers
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.options('*', cors(corsOptions)); // Handle preflight requests
 
-// Otro middleware y configuraciones de tu servidor
-
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// Routes
 app.use("/", routes);
 
-// Escuchar en un puerto específico
+// Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+export default app;
