eadf59e411409476e7cfd45e38f4a630d082020c
Kai <159480405+DarkKay007@users.noreply.github.com>
2024-06-12 13:38:13 -0500

    Update app.js

--- app/app.js
+++ app/app.js
@@ -4,18 +4,28 @@ import cors from 'cors';
 
 const app = express();
 
-// Middleware to enable CORS for specific origin
-app.use(cors({
-  origin: 'https://kuro-gestor.vercel.app', // Allow only this origin
-  credentials: true
-}));
+const allowedOrigins = ['https://kuro-gestor.vercel.app'];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+};
+
+// Enable CORS with options
+app.use(cors(corsOptions));
 
 // Middleware to handle OPTIONS requests
-app.options('*', cors());
+app.options('*', cors(corsOptions));
 
 // Setting headers for all responses
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://kuro-gestor.vercel.app'); // Allow only this origin
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
