c19807fbb9bbbe53d41ce02823aa6235c1f1473b
Kai <159480405+DarkKay007@users.noreply.github.com>
2024-06-12 14:19:04 -0500

    Update app.js

--- app/app.js
+++ app/app.js
@@ -2,31 +2,9 @@ import express from 'express';
 import routes from './routes/index.js';
 import cors from 'cors';
 
-const app = express();
-
-const allowedOrigins = ['https://kuro-gestor.vercel.app'];
-
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,Authorization'
-};
-
-// Enable CORS
-app.use(cors(corsOptions));
-
-// Middleware to handle OPTIONS requests
-app.options('*', cors(corsOptions));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 // Define routes
 app.use("/", routes);
