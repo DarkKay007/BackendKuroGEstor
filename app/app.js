c3b091f336171f05dfdc26febc2b910916fb4017
Kai <159480405+DarkKay007@users.noreply.github.com>
2024-06-12 15:48:22 -0500

    Update app.js

--- app/app.js
+++ app/app.js
@@ -2,13 +2,10 @@ import express from 'express';
 import routes from './routes/index.js';
 import cors from 'cors';
 
-// Crea una instancia de Express
 const app = express();
 
-// Lista de orígenes permitidos (tu frontend desplegado en Vercel)
 const allowedOrigins = ['https://kuro-gestor.vercel.app'];
 
-// Opciones de CORS
 const corsOptions = {
   origin: (origin, callback) => {
     if (!origin || allowedOrigins.includes(origin)) {
@@ -17,30 +14,23 @@ const corsOptions = {
       callback(new Error('Not allowed by CORS'));
     }
   },
-  credentials: true, // Habilita el envío de cookies y otros credenciales
+  credentials: true,
 };
 
-// Habilita CORS con las opciones configuradas
 app.use(cors(corsOptions));
-
-// Middleware para manejar las solicitudes OPTIONS
 app.options('*', cors(corsOptions));
 
-// Middleware para establecer cabeceras en todas las respuestas
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', req.headers.origin); // Establece el origen dinámicamente
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+  res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
   next();
 });
 
-// Middleware para manejar datos JSON y URL codificados
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Usa las rutas definidas
 app.use("/", routes);
 
-// Exporta la instancia de la aplicación Express
 export default app;
