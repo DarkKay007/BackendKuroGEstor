fd9db49b29fc09a72bd8346bdaa0537ee0918cd2
Kai <159480405+DarkKay007@users.noreply.github.com>
2024-06-12 22:47:09 -0500

    Update app.js

--- app/app.js
+++ app/app.js
@@ -25,7 +25,7 @@ app.use('/', routes);
 app.options('*', cors(corsOptions));
 
 // Iniciar el servidor
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000; // Usar el puerto proporcionado por Render o el puerto 5000 como fallback
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
