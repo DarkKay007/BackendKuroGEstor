5958ab366da9712819a3f74c9c584025f35da6cb
Kai <159480405+DarkKay007@users.noreply.github.com>
2024-06-13 07:44:27 -0500

    Update index.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { Router } from 'express';
-import cookieParser from 'cookie-parser';
+
 
 /**
  * !Importaciones de las rutas de los Usuarios
@@ -10,11 +10,10 @@ import routerProjects from './proyectos.routes.js';
 import routerTasks from './tareas.routes.js';
 import routerMeeting from './reuniones.routes.js';
 
-const app = express();
+
 const route = Router(); // Crea una instancia de Router
 
-// Configura cookie-parser
-app.use(cookieParser());
+
 
 route.use("/api", routerTasks)
 route.use("/api", routerUsers);
