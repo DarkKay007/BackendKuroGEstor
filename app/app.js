15e7d6034ca84eab60ca7e894aa3db6dcd8a56d7
Kai <159480405+DarkKay007@users.noreply.github.com>
2024-06-13 13:34:21 -0500

    Update app.js

--- app/app.js
+++ app/app.js
@@ -5,7 +5,7 @@ import cors from 'cors';
 const app = express();
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,PATCH,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
   next();
