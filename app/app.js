4332b1460229f73819a223dd11ae4eefb02fa414
Kai <159480405+DarkKay007@users.noreply.github.com>
2024-06-13 07:49:52 -0500

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,24 +1,29 @@
 import express from 'express';
-import routes from './routes/index.js';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import routes from './routes/index.js';
+
 
 const app = express();
-
-const corsOptions = {
-  origin: 'https://kuro-gestor.vercel.app',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-  credentials: true,
-  allowedHeaders: ['Content-Type', 'Authorization']
-};
-
-app.use(cors(corsOptions)); // Usar el middleware de CORS antes de cualquier otra cosa
 
 // Middleware para parsear JSON y URL-encoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Rutas de tu aplicación
+// Configura cookie-parser
+app.use(cookieParser());
+
+// Configuración de CORS
+const corsOptions = {
+  origin: 'https://kuro-gestor-iheilkmqv-darkkay007s-projects.vercel.app/',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization', 'Access-Control-Allow-Origin', 'Access-Control-Allow-Credentials']
+};
 app.use('/', routes);
+
+// Aplicar middleware de CORS globalmente
+app.use(cors(corsOptions));
 
 // Manejo de solicitudes preflight (OPTIONS)
 app.options('*', cors(corsOptions));
@@ -26,7 +31,7 @@ app.options('*', cors(corsOptions));
 // Iniciar el servidor
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(Server running on port ${PORT});
 });
 
 export default app;
