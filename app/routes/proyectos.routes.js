aff01f7522519df396528c0dfe8a147ce7518ab5
Kai <159480405+DarkKay007@users.noreply.github.com>
2024-06-13 07:42:39 -0500

    Update proyectos.routes.js

--- app/routes/proyectos.routes.js
+++ app/routes/proyectos.routes.js
@@ -1,21 +1,13 @@
 import { Router } from "express";
 import { validarPermiso } from "../middlewares/usuarios.middlewares.js";
 import { delProject, getProject, postProject, putProject, showProject } from "../controllers/proyectos.controllers.js";
-import cors from 'cors';
 
 const routerProjects = Router();
-const corsOptions = {
-  origin: 'https://kuro-gestor.vercel.app',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-  credentials: true,
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  exposedHeaders: ['Access-Control-Allow-Origin'],
-  preflightContinue: true
-};
-routerProjects.get("/proyectos", cors(corsOptions),validarPermiso ,  showProject);
-routerProjects.get("/proyecto/:nombre", cors(corsOptions),validarPermiso ,  getProject);
-routerProjects.post("/proyecto", cors(corsOptions),validarPermiso ,  postProject);
-routerProjects.put("/proyecto", cors(corsOptions),validarPermiso ,  putProject);
-routerProjects.delete("/proyecto/:nombre", cors(corsOptions),validarPermiso ,   delProject);
+
+routerProjects.get("/proyectos",validarPermiso ,  showProject);
+routerProjects.get("/proyecto/:nombre",validarPermiso ,  getProject);
+routerProjects.post("/proyecto",validarPermiso ,  postProject);
+routerProjects.put("/proyecto",validarPermiso ,  putProject);
+routerProjects.delete("/proyecto/:nombre",validarPermiso ,   delProject);
 
 export default routerProjects;
