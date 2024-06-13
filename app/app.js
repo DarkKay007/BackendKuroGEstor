e95c444c245bb0d7e61f6154bd499ff617da702f
Kai <159480405+DarkKay007@users.noreply.github.com>
2024-06-13 09:51:13 -0500

    Update app.js

--- app/app.js
+++ app/app.js
@@ -23,15 +23,6 @@ app.use('/', routes);
 // Manejo de solicitudes preflight (OPTIONS)
 app.options('*', cors(corsOptions));
 
-// Configurar cabeceras CORS en las respuestas
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://kuro-gestor.vercel.app');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  next();
-});
-
 // Iniciar el servidor
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
