047141a39f621720f01653250d082384e6ae4795
juancho123344 <juanjosesilver1@gmail.com>
2024-06-13 17:17:39 -0500

    Update reuniones.routes.js

--- app/routes/reuniones.routes.js
+++ app/routes/reuniones.routes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { validarPermiso } from '../middlewares/usuarios.middlewares.js';
-import { delReunion, postReunion, putReunion, showReuniones } from '../controllers/reuniones.controller.js';
+import { delReunion, postReunion, putReunion, showReuniones,showAllMeetings  } from '../controllers/reuniones.controller.js';
 
 const routerMeeting = Router();
 
@@ -9,5 +9,7 @@ routerMeeting.get("/meetings", showReuniones);
 routerMeeting.post("/meeting", postReunion);
 routerMeeting.put("/meeting/:reunion_id", putReunion);
 routerMeeting.delete("/meeting/:reunion_id", delReunion);
+routerMeeting.get('/meetings/all', showAllMeetings); // Nueva ruta para mostrar todas las reuniones
+
 
 export default routerMeeting;
