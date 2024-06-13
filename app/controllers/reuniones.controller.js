c817d1ec30b1092444c764b1ef179444e35a6c1e
juancho123344 <juanjosesilver1@gmail.com>
2024-06-13 17:08:24 -0500

    Update reuniones.controller.js

--- app/controllers/reuniones.controller.js
+++ app/controllers/reuniones.controller.js
@@ -18,17 +18,16 @@ export const showReuniones = async (req, res) => {
     res.status(500).json({ error: error.message, resultado: 'Error en la consulta Get de reuniones' });
   }
 };
-
 export const postReunion = async (req, res) => {
   const { nombre, descripcion, fecha_inicio, fecha_fin } = req.body;
 
   try {
     const resultado = await pool.query(
-      'INSERT INTO reuniones (nombre, descripcion, fecha_inicio, fecha_fin) VALUES (?, ?, ?, ?, ?)',
-      [nombre, descripcion, fecha_inicio, fecha_fin]
+      'INSERT INTO reuniones (nombre, descripcion, fecha_inicio, fecha_fin) VALUES (?, ?, ?, ?)',
+      [nombre, descripcion, fecha_inicio, fecha_fin] // Aquí hay un error, debería ser solo cuatro signos de interrogación ya que son cuatro los datos en inserción
     );
 
-    if (resultado[0].affectedRows > 0) {
+    if (resultado.affectedRows > 0) { // Aquí no es [0].affectedRows sino que es resultado.affectedRows
       res.json({ resultado: 'Reunión creada exitosamente' });
     } else {
       res.json({ resultado: 'Reunión no creada' });
@@ -38,7 +37,6 @@ export const postReunion = async (req, res) => {
     res.status(500).json({ error: error.message, resultado: 'Error al crear reunión' });
   }
 };
-
 export const putReunion = async (req, res) => {
   const { reunion_id } = req.params;
   const { nombre, descripcion, fecha_inicio, fecha_fin } = req.body;
@@ -54,7 +52,7 @@ export const putReunion = async (req, res) => {
       [nombre, descripcion, fecha_inicio, fecha_fin, date_update, reunion_id]
     );
 
-    if (resultado[0].affectedRows > 0) {
+    if (resultado.affectedRows > 0) { // Aquí también no es resultado[0].affectedRows sino que es resultado.affectedRows
       res.json({ resultado: 'Reunión actualizada exitosamente' });
     } else {
       res.json({ resultado: 'Reunión no actualizada' });
