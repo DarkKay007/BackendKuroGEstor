0bed0fc37780608565416f92c8bdb8936f1ecd37
Kai <159480405+DarkKay007@users.noreply.github.com>
2024-06-13 07:54:45 -0500

    Server running on port ${PORT}

--- app/app.js
+++ app/app.js
@@ -31,7 +31,7 @@ app.options('*', cors(corsOptions));
 // Iniciar el servidor
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(Server running on port ${PORT});
+  console.log('Server running on port ${PORT}');
 });
 
 export default app;
