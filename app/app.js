ce5b0d3095eb2da9b4235ad410260d19bef97b9a
Kai <159480405+DarkKay007@users.noreply.github.com>
2024-06-13 07:17:13 -0500

    123

--- app/app.js
+++ app/app.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: false }));
 // Configura cookie-parser
 app.use(cookieParser());
 
-// Configuración de CORS
+
 const corsOptions = {
   origin: 'https://kuro-gestor.vercel.app',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
@@ -19,15 +19,8 @@ const corsOptions = {
   allowedHeaders: ['Content-Type', 'Authorization']
 };
 
-// Aplicar middleware de CORS globalmente
-app.use(cors(corsOptions));
-
-// Aplicar middleware de CORS a las rutas específicas
-
-// Manejo de solicitudes preflight (OPTIONS)
 app.options('*', cors(corsOptions));
 
-// Iniciar el servidor
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
