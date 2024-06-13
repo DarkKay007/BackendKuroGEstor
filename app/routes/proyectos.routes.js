05903bcf0ea02398de4e9b07f6bbe8df42263e5f
Kai <159480405+DarkKay007@users.noreply.github.com>
2024-06-12 23:05:18 -0500

    Update proyectos.routes.js

--- app/routes/proyectos.routes.js
+++ app/routes/proyectos.routes.js
@@ -8,7 +8,9 @@ const corsOptions = {
   origin: 'https://kuro-gestor.vercel.app',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   credentials: true,
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  exposedHeaders: ['Access-Control-Allow-Origin'],
+  preflightContinue: true
 };
 routerProjects.get("/proyectos", cors(corsOptions),validarPermiso ,  showProject);
 routerProjects.get("/proyecto/:nombre", cors(corsOptions),validarPermiso ,  getProject);
