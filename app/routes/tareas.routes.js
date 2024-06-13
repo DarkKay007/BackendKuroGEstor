ccdedcf5369137fbb1e4b9c6eccf89c96faf7c9a
Kai <159480405+DarkKay007@users.noreply.github.com>
2024-06-13 13:48:51 -0500

    Update tareas.routes.js rutas no protegidas

--- app/routes/tareas.routes.js
+++ app/routes/tareas.routes.js
@@ -3,10 +3,10 @@ import { validarPermiso } from "../middlewares/usuarios.middlewares.js";
 import { delTask, getTask, postTask, putTask, showTasks } from "../controllers/tareas.controllers.js";
 const routerTasks = Router();
 
-routerTasks.get("/tasks", validarPermiso, showTasks );
-routerTasks.get("/task/:nombre", validarPermiso, getTask);
-routerTasks.post("/task", validarPermiso, postTask);
-routerTasks.put("/task", validarPermiso, putTask);
-routerTasks.delete("/task/:nombre", validarPermiso, delTask);
+routerTasks.get("/tasks", showTasks );
+routerTasks.get("/task/:nombre", getTask);
+routerTasks.post("/task", postTask);
+routerTasks.put("/task", putTask);
+routerTasks.delete("/task/:nombre", delTask);
 
 export default routerTasks;
