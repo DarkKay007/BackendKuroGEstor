89821dc40554ac56a1535bd260c1a5be3b63c8fc
Kai <159480405+DarkKay007@users.noreply.github.com>
2024-06-12 23:01:40 -0500

    Update tareas.routes.js

--- app/routes/tareas.routes.js
+++ app/routes/tareas.routes.js
@@ -2,11 +2,17 @@ import { Router } from "express";
 import { validarPermiso } from "../middlewares/usuarios.middlewares.js";
 import { delTask, getTask, postTask, putTask, showTasks } from "../controllers/tareas.controllers.js";
 const routerTasks = Router();
-
-routerTasks.get("/tasks", validarPermiso, showTasks );
-routerTasks.get("/task/:nombre", validarPermiso, getTask);
-routerTasks.post("/task", validarPermiso, postTask);
-routerTasks.put("/task", validarPermiso, putTask);
-routerTasks.delete("/task/:nombre", validarPermiso, delTask);
+import cors from 'cors';
+const corsOptions = {
+  origin: 'https://kuro-gestor.vercel.app',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
+routerTasks.get("/tasks", cors(corsOptions), validarPermiso, showTasks );
+routerTasks.get("/task/:nombre", cors(corsOptions), validarPermiso, getTask);
+routerTasks.post("/task", cors(corsOptions), validarPermiso, postTask);
+routerTasks.put("/task", cors(corsOptions), validarPermiso, putTask);
+routerTasks.delete("/task/:nombre", cors(corsOptions), validarPermiso, delTask);
 
 export default routerTasks;
