2c4a8e92d661aa90b75d9fb96fb720c99eb56a11
Kai <159480405+DarkKay007@users.noreply.github.com>
2024-06-13 07:38:17 -0500

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,17 +1,8 @@
 import express from 'express';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
 import routes from './routes/index.js';
+import cors from 'cors';
 
 const app = express();
-
-// Middleware para parsear JSON y URL-encoded data
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-// Configura cookie-parser
-app.use(cookieParser());
-
 
 const corsOptions = {
   origin: 'https://kuro-gestor.vercel.app',
@@ -20,9 +11,19 @@ const corsOptions = {
   allowedHeaders: ['Content-Type', 'Authorization']
 };
 
-app.options('*', cors(corsOptions));
+app.use(cors(corsOptions)); // Usar el middleware de CORS antes de cualquier otra cosa
+
+// Middleware para parsear JSON y URL-encoded data
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Rutas de tu aplicación
 app.use('/', routes);
 
+// Manejo de solicitudes preflight (OPTIONS)
+app.options('*', cors(corsOptions));
+
+// Iniciar el servidor
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
