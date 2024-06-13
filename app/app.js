44d4faba0fc76c0c5ea87e9b1909e0cfa23e99b4
Kai <159480405+DarkKay007@users.noreply.github.com>
2024-06-12 23:23:07 -0500

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,31 +1,43 @@
 import express from 'express';
-import routes from './routes/index.js';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+// Importaciones de las rutas
+import routerUsers from './usuarios.routes.js';
+import routerProjects from './proyectos.routes.js';
+import routerTasks from './tareas.routes.js';
+import routerMeeting from './reuniones.routes.js';
 
 const app = express();
 
-const corsOptions = {
-  origin: 'hhttps://kuro-gestor-iheilkmqv-darkkay007s-projects.vercel.app',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-  credentials: true,
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  exposedHeaders: ['Access-Control-Allow-Origin'],
-  preflightContinue: true
-};
-app.options('*', cors())
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://kuro-gestor-iheilkmqv-darkkay007s-projects.vercel.app");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-app.use(cors(corsOptions)); // Use the cors middleware
-
+// Middleware para parsear JSON y URL-encoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/', routes);
-app.options('/api/tasks/*', cors(corsOptions));
-app.options('/api/meetings/*', cors(corsOptions));
+// Configura cookie-parser
+app.use(cookieParser());
+
+// Configuración de CORS
+const corsOptions = {
+  origin: 'https://kuro-gestor-iheilkmqv-darkkay007s-projects.vercel.app',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
+
+// Aplicar middleware de CORS globalmente
+app.use(cors(corsOptions));
+
+// Aplicar middleware de CORS a las rutas específicas
+app.use('/api/tasks', cors(corsOptions), routerTasks);
+app.use('/api/meetings', cors(corsOptions), routerMeeting);
+app.use('/api/users', cors(corsOptions), routerUsers);
+app.use('/api/projects', cors(corsOptions), routerProjects);
+
+// Manejo de solicitudes preflight (OPTIONS)
+app.options('*', cors(corsOptions));
+
+// Iniciar el servidor
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
