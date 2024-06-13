bced9a7a8e5cf565b0d9fbcadc0a0897ae3e48fc
Kai <159480405+DarkKay007@users.noreply.github.com>
2024-06-12 22:53:33 -0500

    BalckBoxapp.js

--- app/app.js
+++ app/app.js
@@ -4,32 +4,22 @@ import cors from 'cors';
 
 const app = express();
 
-// Configuración de CORS
 const corsOptions = {
   origin: 'https://kuro-gestor.vercel.app',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   credentials: true,
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  preflightContinue: true
 };
-app.use(function (request, response, next) {
-  response.header("Access-Control-Allow-Origin", "*");
-  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-app.use(cors(corsOptions)); // Usar el middleware de CORS antes de cualquier otra cosa
 
-// Middleware para parsear JSON y URL-encoded data
+app.use(cors(corsOptions)); // Use the cors middleware
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Rutas de tu aplicación
 app.use('/', routes);
 
-// Manejo de solicitudes preflight (OPTIONS)
-app.options('*', cors(corsOptions));
-
-// Iniciar el servidor
-const PORT = process.env.PORT || 5000; // Usar el puerto proporcionado por Render o el puerto 5000 como fallback
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
